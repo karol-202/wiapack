284a063ae786bfdc53b5fea231ee25641cfa22e5
karol-202 <karoljurski1@gmail.com>
2020-04-07 22:02:41 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ mkdir "$script_dst" 2> /dev/null
 cp "$(dirname "$0")/$script_name" "$script_dst/$script_name"
 
 if [[ -f "$link_dst/$link_name" ]]; then
-  sudo rm "$link_dst/$link_name"
+  rm "$link_dst/$link_name"
 fi
 ln -s "$script_dst/$script_name" "$link_dst/$link_name"
